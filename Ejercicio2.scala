bf93e104a9c8c2e96fd4a520640ad8b4a34b299d
Diana Laura Huerta Navarro <diana_170309@hotmail.com>
2015-09-09 15:33:06 -0500

    Pasos para realizar un PR

---
+++ Ejercicio2.scala
@@ -0,0 +1,9 @@
+println ("Ingresa presuspuesto total anual")
+val PresupuestoTotalAnual=readFloat()
+
+val DineroGinecologia=(PresupuestoTotalAnual*.40)
+val DineroTraumatologia=(PresupuestoTotalAnual*.30)
+val DineroPediatria=(PresupuestoTotalAnual*.30)
+println  ("el presupuesto para ginecologia es " + DineroGinecologia)
+println  ("el presupuesto para traumatologia es " + DineroTraumatologia)
+println  ("el presupuesto para pediatria es " + DineroPediatria)
