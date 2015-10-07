b1bab006cab31905d68f475c7c5eae38367b1b1c
Diana Laura <diana_170309@hotmail.com>
2015-10-07 12:46:51 -0500

    Ejercicio 4 del Examen

---
+++ 4.Expendio.scala
@@ -0,0 +1,22 @@
+﻿Var Kilos = 0
+var TotalCliente = 0
+var TotalTienda = 0
+
+println ("¿Cual es el precio del kilo de naranja?
+kilonaranja = readFloat ()
+
+for (i <- 1 to 15) {
+println ¿Cuantos kilos de naranja compraste?
+kilos = readFloat ()
+TotalCliente = kilonaranja * kilos
+
+if (kilos > 10){
+	TotalCliente = TotalCliente * 0.85
+}
+
+TotalTienda += TotalCliente
+
+println ("Usted va a pagar: " + TotalCliente)
+}
+
+println ("La tienda ganara: " + TotalTienda)
