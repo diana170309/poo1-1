c84c3c4e1e930b3a34e0aa35eb7247ccf81240bd
Diana Laura <diana_170309@hotmail.com>
2015-10-16 08:12:19 -0500

    Coreccion Ejercicio2-Examen1

---
+++ Calificaciones.2.scala
@@ -0,0 +1,16 @@
+var menor = 11.0
+var calificacion = 0.0
+
+for (i <- 1 to 40) {
+
+println ("Ingresa tu calificacion: ") 
+calificacion = readFloat ()
+
+
+if (calificacion < menor ) {
+menor = calificacion
+}
+
+}
+ println ("La calificacion menor obtenida es: " + menor)
+ 
